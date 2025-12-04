351abc35c7e75537299003b1802919005b8d899f
CarlhwigoGorbsknrman <m_garanin_218613790@rambler.ru>
2025-12-04 08:01:53 +0800

    add mutations-fixture-middleware.py, template.py

---
+++ frontend/web/src/adapter-main/template.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\adapter-main\template.py
+// Generated at: 2025-10-01T12:55:47.919Z
---
+++ mutations-fixture-middleware.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\mutations-fixture-middleware.py
+// Generated at: 2025-10-01T12:55:47.905Z
